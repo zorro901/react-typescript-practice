e346a84dac1f84bf747cc940fcac2b86b8a89e1a
zorro901 <zoro907@gmail.com>
2021-08-09 03:42:05 +0900

    53. 型定義を効率的に管理しよう

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
-
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
+import { TodoType } from "./types/todo";
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -26,6 +20,7 @@ export default function App() {
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo
+          key={todo.id}
           title={todo.title}
           userId={todo.userId}
           completed={todo.completed}
