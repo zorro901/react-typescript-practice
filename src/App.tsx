a25c09bc8e4032d88d1fbf96e9949a85ac7d93a3
zorro901 <zoro907@gmail.com>
2021-08-09 03:09:00 +0900

    50. 型がないせいでバグっているアプリの例

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
+import { Todo } from "./Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
   const onClickFetchData = () => {
     axios
       .get("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => console.log(res));
+      .then((res) => setTodos(res.data));
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
     </div>
   );
 }
